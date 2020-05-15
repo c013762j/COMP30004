aa9851101fcf4bebfd743969a47fc9d671d0fb23
c013762j <65301968+c013762j@users.noreply.github.com>
2020-05-15 05:56:37 +0100

    newer fruit machine page files

--- fruit.js
+++ fruit.js
@@ -1,1 +1,36 @@
-console.log("hello");
+const machineForm = document.getElementById("slotForm");
+
+const creditValue = document.getElementById("cvalue");
+
+const winningsValue = document.getElementById("wvalue");
+
+const btnCredit = document.getElementById("credit");
+
+
+
+let creditval = 0;
+
+let winningval = 0;
+
+
+creditValue.innerText = `${creditval}`;
+winningsValue.innerText = `${winningval}`;
+
+btnCredit.addEventListener("click", calculatecredit);
+
+function calculatecredit() {
+
+    if (machineForm.checkValidity()) {
+        event.preventDefault();
+
+        let creditval = creditval + 1;
+
+        
+
+        
+
+    }
+
+}
+
+
